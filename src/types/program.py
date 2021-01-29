9a1ff56ba837c696341ccc334224e006e49fee22
Arvid Norberg <arvid@libtorrent.org>
2021-01-29 17:14:16 +0100

    introduce SerializedProgram to use in the FullNode. Instead of parsing block programs in python, maintain them in serialized form to save time when executing them in rust

--- src/types/program.py
+++ src/types/program.py
@@ -12,6 +12,11 @@ from clvm.EvalError import EvalError
 
 from clvm_tools.curry import curry, uncurry
 
+try:
+    from clvm_rs import serialize_and_run_program
+except ImportError:
+    serialize_and_run_program = None
+
 
 def run_program(
     program,
@@ -102,3 +107,73 @@ class Program(SExp):
         return type(self).from_bytes(bytes(self))
 
     EvalError = EvalError
+
+
+def _tree_hash(node: SExp, precalculated: Set[bytes32]) -> bytes32:
+    """
+    Hash values in `precalculated` are presumed to have been hashed already.
+    """
+    if node.listp():
+        left = _tree_hash(node.first(), precalculated)
+        right = _tree_hash(node.rest(), precalculated)
+        s = b"\2" + left + right
+    else:
+        atom = node.as_atom()
+        if atom in precalculated:
+            return bytes32(atom)
+        s = b"\1" + atom
+    return bytes32(std_hash(s))
+
+
+class SerializedProgram:
+    """
+    An opaque representation of a clvm program. It has a more limited interface than a full SExp
+    """
+
+    _buf: bytes = b""
+
+    @classmethod
+    def parse(cls, f) -> "SerializedProgram":
+        tmp = sexp_from_stream(f, SExp.to)
+        return SerializedProgram.from_bytes(tmp.as_bin())
+
+    def stream(self, f):
+        f.write(self._buf)
+
+    @classmethod
+    def from_bytes(cls, blob: bytes) -> "SerializedProgram":
+        ret = SerializedProgram()
+        ret._buf = bytes(blob)
+        return ret
+
+    def __bytes__(self) -> bytes:
+        return self._buf
+
+    def __str__(self) -> str:
+        return bytes(self).hex()
+
+    def get_tree_hash(self, *args: List[bytes32]) -> bytes32:
+        """
+        Any values in `args` that appear in the tree
+        are presumed to have been hashed already.
+        """
+        print(self._buf)
+        print(type(self._buf))
+        assert type(self._buf) == bytes
+        tmp = sexp_from_stream(io.BytesIO(self._buf), SExp.to)
+        return _tree_hash(tmp, set(args))
+
+    def run_with_cost(self, args) -> Tuple[int, SExp]:
+        assert type(self._buf) == bytes
+        if type(args) == SerializedProgram:
+            prog_args = args._buf
+            assert type(args._buf) == bytes
+        else:
+            prog_args = SExp.to(args).as_bin()
+        max_cost = 0
+        cost, ret = serialize_and_run_program(self._buf, prog_args, 1, 3, max_cost)
+        return cost, sexp_from_stream(io.BytesIO(ret), SExp.to)
+
+    def run(self, args) -> "Program":
+        cost, r = self.run_with_cost(args)
+        return Program.to(r)
